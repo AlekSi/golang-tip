6135e87d7cc11dd82c5fda93aef666bd1764e55c
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2021-06-13 20:03:29 +0300

    WIP

--- tipper.go
+++ tipper.go
@@ -1,62 +1,129 @@
 package main
 
 import (
+	"crypto/sha256"
 	"flag"
+	"fmt"
+	"io"
 	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"sync"
 	"time"
 )
 
+const (
+	checksumFileName = "sha256.txt"
+)
+
+func checksumFile(path string) (string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err = io.Copy(h, f); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%064x", h.Sum(nil)), nil
+}
+
 func createChecksumFile() {
-	err := filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
+	paths := make(chan string)
+	lines := make(chan string)
 
-		if info.IsDir() {
-			if path == ".git" {
-				return filepath.SkipDir
+	go func() {
+		err := filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return err
 			}
+
+			if info.IsDir() {
+				if path == ".git" {
+					return filepath.SkipDir
+				}
+				return nil
+			}
+
+			if !strings.HasSuffix(path, ".tar.gz") {
+				return nil
+			}
+
+			paths <- path
 			return nil
+		})
+		if err != nil {
+			log.Fatal(err)
 		}
+		close(paths)
+	}()
 
-		if !strings.HasSuffix(path, ".tar.gz") {
-			return nil
-		}
+	var wg sync.WaitGroup
+	for i := 0; i < runtime.GOMAXPROCS(-1); i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
 
-		log.Print(path)
+			for path := range paths {
+				checksum, err := checksumFile(path)
+				if err != nil {
+					log.Fatal(err)
+				}
+				lines <- fmt.Sprintf("%s  %s", checksum, path)
+			}
+		}()
+	}
 
-		return nil
-	})
+	go func() {
+		wg.Wait()
+		close(lines)
+	}()
+
+	f, err := os.Create(checksumFileName)
 	if err != nil {
 		log.Fatal(err)
 	}
+	for line := range lines {
+		if _, err = f.WriteString(line + "\n"); err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err = f.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%s created.", checksumFileName)
 }
 
 func createTimestampFile() {
-	name := time.Now().UTC().Format(time.RFC3339) + ".txt"
+	name := fmt.Sprintf("Built on %s.txt", time.Now().UTC().Format(time.RFC3339))
 	if err := os.WriteFile(name, nil, 0o644); err != nil {
 		log.Fatal(err)
 	}
+
+	log.Printf("%q created.", name)
 }
 
 func main() {
 	log.SetPrefix("tipper: ")
 	log.SetFlags(log.Lmsgprefix)
 
-	checksumF := flag.Bool("checksum", false, "create checksum file")
-	timestampF := flag.Bool("timestamp", false, "create timestamp file")
-
 	flag.Parse()
 
-	if *checksumF {
-		createChecksumFile()
-	}
-
-	if *timestampF {
-		createTimestampFile()
+	for _, cmd := range flag.Args() {
+		switch cmd {
+		case "checksum":
+			createChecksumFile()
+		case "timestamp":
+			createTimestampFile()
+		default:
+			log.Fatalf("Unhandled command %q.", cmd)
+		}
 	}
 }
